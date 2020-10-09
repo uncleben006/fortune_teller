13536626a253a2da8dfcb35f948d78131e2cfc9b
uncleben006 <uncleben006@gmail.com>
2020-10-10 01:41:45 +0800

    Update command file

--- command.sh
+++ command.sh
@@ -1,18 +1,21 @@
 
 ########## local develop with docker ############
 
+# Pull and Run postgres container
+docker run --name postgres -p 172.17.0.2:5432:5432 -e POSTGRES_PASSWORD=1234 -d postgres
+
 # Build image base on Dockerfile
-docker build . -t uncleben006/fortune_telling
+docker build . -t uncleben006/fortune_teller
 
 # Run container in daemon and mount project
-docker run -it -d --name fortune_telling \
+docker run -it -d --name fortune_teller \
 -p 443:443 -p 80:80 \
 -e LC_ALL=C.UTF-8 \
--v /media/ben_wang/ben/github/fortune_telling/:/usr/src/app \
-uncleben006/fortune_telling bash
+-v /media/ben_wang/ben/github/fortune_teller/:/usr/src/app \
+uncleben006/fortune_teller bash
 
 # Execute container
-docker exec -it fortune_telling bash
+docker exec -it fortune_teller bash
 
 # Run uwsgi
 # Bind soft link for project Nginx config
@@ -29,13 +32,13 @@ service nginx start
 ########## Deploy to the cloud with docker ############
 
 # push to my docker hub
-docker push uncleben006/fortune_telling
+docker push uncleben006/fortune_teller
 
 # pull and run on the cloud
-docker run -it -d --name fortune_telling \
+docker run -it -d --name fortune_teller \
 -p 80:80 -p 443:443 \
 -e LC_ALL=C.UTF-8 \
-uncleben006/fortune_telling bash
+uncleben006/fortune_teller bash
 
 ########## Deploy to the cloud with docker ############
 
